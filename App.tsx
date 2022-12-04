d8a30750d2fd9e118555cae88f06d6e084e50a79
HelLuv <works.person@gmail.com>
2022-12-04 14:11:55 +0300

    added redux, store, authSlice

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
+import {Provider} from "react-redux";
 
 import Navigation from "./src/navigation";
+import store from "./src/store";
 
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navigation/>
       <StatusBar style="auto"/>
-    </>
+    </Provider>
   );
 }
 
